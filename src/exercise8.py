b38e6a79ef272910b328d6155f2daa001670af16
xyapple <ydeascentis@gmail.com>
2016-08-04 18:30:16 -0400

    Python 练习题 2016-08-04 输入一个数，判断这个数是否为素数

---
+++ src/exercise8.py
@@ -0,0 +1,24 @@
+num = int(input('please enter a number'))
+
+def prime_numer_test():
+
+    total = 0
+    factors = range(1, num)
+    cfactors = range(1, num + 1)
+
+    if num <= 1:
+        print('{0:d} is not a prime'.format(num))
+    elif num == 2:
+        print('{0:d} is a prime number'.format(num))
+    elif num % 2 == 0:
+        print('{0:d} is NOT a prime number'.format(num))
+    else:
+        for i in cfactors:
+            if (num % i ) == 0:
+                total += i
+        if total != sum(factors):
+            print('{0:d} is a prime'.format(num))
+        else:
+            print('{0:d} is NOT a prime'.format(num))
+
+prime_numer_test()
