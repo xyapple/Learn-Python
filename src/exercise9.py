f758c10e4d2ca6d6aae244d557b4151f31762232
xyapple <ydeascentis@gmail.com>
2016-08-04 20:25:34 -0400

    Python 练习题 2016-08-04 输入一个时间（小时:分钟:秒），输出该时间经过9分30秒后的时间。

---
+++ src/exercise9.py
@@ -0,0 +1,25 @@
+import datetime
+
+# def add_time(userH, userM, userS, hour, min, sec):
+userH = int(input('enter a hour'))
+userM = int(input('enter a minute'))
+userS = int(input('enter a second'))
+userTime = datetime.time(userH, userM, userS)
+print(userTime)
+
+sec = userS + 30
+mic = userM +9
+if sec >= 60:
+    sec = sec - 60
+    mic1 = mic + 1
+    if mic1 >= 60:
+        mic = mic1 - 60
+        userH = userH + 1
+
+addTime = datetime.time(userH, mic, sec)
+print(addTime)
+
+
+
+
+
