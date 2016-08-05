92f0b6c0606cd90201795d2e6744d20277a0f0d4
xyapple <ydeascentis@gmail.com>
2016-08-04 22:03:53 -0400

    Python 练习题 2016-08-04 定期存款

--- src/exercise6.py
+++ src/exercise6.py
@@ -4,6 +4,6 @@ dollar = 10000
 
 while dollar < 30000:
         year += 1
-        dollar = dollar * (1 + 0.002) **year
+        dollar = dollar * (1 + 0.02) **year
 
 print(str(year) + ' yearly saving will be reaching 30000')
