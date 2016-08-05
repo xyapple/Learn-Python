361bbfa79f1ff4eaa515cc42ef922dadb90fe77a
xyapple <ydeascentis@gmail.com>
2016-08-05 12:51:18 -0400

    Python 练习题 2016-08-04

--- src/exercise7.py
+++ src/exercise7.py
@@ -1,9 +1,18 @@
 import datetime
 
-year = int(input('please enter a year'))
-month = int(input('please enter a month'))
-day = int(input('please enter a day'))
+# year = int(input('please enter a year'))
+# month = int(input('please enter a month'))
+# day = int(input('please enter a day'))
+#
+# date = datetime.date(year, month, day)
+while True:
+    try:
+        date_entry = input('Enter a date in YYYY-MM-DD format')
+        year, month, day = map(int, date_entry.split('-'))
+        date1 = datetime.date(year, month, day)
+    except ValueError:
+        print ('Incorrect format, please try again')
 
-date = datetime.date(year, month, day)
-
-print(date.strftime('%j'))
+        continue
+    else:
+        print(date1.strftime('%j'))
