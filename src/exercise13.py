19a83eac42026b1bbfd58bf6f57c2733e26847f1
xyapple <ydeascentis@gmail.com>
2016-08-13 11:18:22 -0400

    Python Automate # Tic-Tac-Toe Game with Player

---
+++ src/exercise13.py
@@ -0,0 +1,23 @@
+# Tic-Tac-Toe Game with Player by entering name
+
+theBoard = {'top-L' : ' ', 'top-M': ' ', 'top-R':' ',
+            'mid-L' : ' ', 'mid-M': ' ', 'mid-R':' ',
+            'low-L' : ' ', 'low-M':' ', 'low-R':' '}
+print('This is the tic-tac-toe game. \n Please enter (board space) for your move')
+def printBoard(board):
+    print(board['top-L'] +'|' + board['top-M'] +'|'+ board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|'+ board['mid-M'] +'|'+board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] +'|'+board['low-M']+'|'+board['low-R'])
+turn = 'X'
+for i in range(9):
+        printBoard(theBoard)
+        print('Turn for ' + turn + '. Move on which space?')
+        move = input()
+        theBoard[move] = turn
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+printBoard(theBoard)
